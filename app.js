181282a17e807cc87dec7f5184a9bfda473a0683
Alishan Atashev <alishanatasheva@gmail.com>
2023-04-18 19:40:31 +0600

    Edited README.md File

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const app = express()
 
+const db = require("./database")
+
 app.use(express.static("public"))
+
+app.get("/app", (req,res) => {
+    db.query("USE coding_bros;")
+    res.send("Hello World!")
+})
 
 app.listen(3000, () => {
     console.log("listening on 3000")

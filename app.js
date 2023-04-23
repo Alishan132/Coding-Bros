7553decf80b15064deb1431cc7d0196157406bce
Alishan Atashev <alishanatasheva@gmail.com>
2023-04-23 23:39:51 +0600

    Connected EVERYTHING

--- app.js
+++ app.js
@@ -2,12 +2,43 @@ const express = require("express")
 const app = express()
 
 const db = require("./database")
+const knex = require("./knex")
 
-app.use(express.static("public"))
+app.use(express.static("public", { type: 'text/css' }))
+app.set("view engine", "ejs")
 
-app.get("/app", (req,res) => {
-    db.query("USE coding_bros;")
-    res.send("Hello World!")
+
+function doQuery (queryStr){
+    return new Promise((resolve, reject) => {
+        db.query(queryStr, (err, data) => {
+            if(err) reject(err)
+            else resolve(data)
+        })
+    })
+}
+
+app.get("/", async (req,res) => {
+    let where = {}
+    if(req.query.city != null && req.query.city != ""){
+        where.city = req.query.city
+    }
+    if(req.query.rating != null && req.query.rating){
+        where.rating = req.query.rating
+    }
+    const hotels = await knex.select("*").from("hotels").where(where)
+    const uniqueCities = await knex.select("city").distinct().from("hotels")
+    const cities = uniqueCities.map(hotel => {
+            return hotel.city
+    }) 
+    res.render("index", {hotels : hotels, searchValues : req.query, cities: cities})
+})
+
+app.get("/signin", (req, res) => {
+    res.render("signin")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
 })
 
 app.listen(3000, () => {
--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 require("dotenv").config()
-const mysql = require("mysql")
+// const mysql = require("mysql")
 
-const connection = mysql.createConnection({
+const connection = {
     host : process.env.MYSQL_HOST,
     user : process.env.MYSQL_USER,
     password : process.env.MYSQL_PASSWORD,
     database : "coding_bros",
-    port : 3306
-})
+}
 
 module.exports = connection
